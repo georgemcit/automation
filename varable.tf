4a8966aadafef96193db73a97b09ffd264519b4f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-16 22:06:47 -0400

    Update varable.tf

--- varable.tf
+++ varable.tf
@@ -1,6 +1,5 @@
-/*
 locals{
-  window_app=[for f in fileset("${path.module}/${var.windowsconfig}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.windowsconfig}/${f}"))]
+  window_app=[for f in fileset("${path.module}/${var.dev}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.dev}/${f}"))]
   window_app_list = flatten([
     for app in local.window_app : [
       for windowapps in try(app.listofwindowsapp, []) :{
@@ -12,23 +11,23 @@ locals{
     ]
 ])
 }
-resource "azurerm_service_plan" "windowsbatcha06sp" {
+resource "azurerm_service_plan" "george" {
   for_each            ={for sp in local.window_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
 
-resource "azurerm_linux_web_app" "windowsbatcha06webapp" {
-  for_each            = azurerm_service_plan.windowsbatcha06sp
+resource "azurerm_window_app" "george1980" {
+  for_each            = azurerm_service_plan.george
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.george_ibrahim.name
+  location            = azurerm_resource_group.george_ibrahim.location
   service_plan_id     = each.value.id
 
 
   site_config {}
 }
-*/
+
