78ceb2cd9ff6516c33d233454b7ea868b2095f62
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-16 22:42:23 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,22 +12,18 @@ locals{
 ])
 }
 resource "azurerm_service_plan" "george" {
-  for_each            ={for sp in local.window_app_list: "${sp.name}"=>sp }
-  name                = each.value.name
+  name                = "george"
   resource_group_name = azurerm_resource_group.george_ibrahim.name
   location            = azurerm_resource_group.george_ibrahim.location
-  os_type             = each.value.os_type
-  sku_name            = each.value.sku_name
+  sku_name            = "P1v2"
+  os_type             = "Windows"
 }
 
 resource "azurerm_windows_web_app" "george1980" {
-  for_each            = azurerm_service_plan.george
-  name                = each.value.name
+  name                = "george1980"
   resource_group_name = azurerm_resource_group.george_ibrahim.name
-  location            = azurerm_resource_group.george_ibrahim.location
-  service_plan_id     = each.value.id
-
+  location            = azurerm_service_plan.george_ibrahim.location
+  service_plan_id     = azurerm_service_plan.george.id
 
   site_config {}
 }
-
